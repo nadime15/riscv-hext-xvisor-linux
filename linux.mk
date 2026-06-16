f7b4abcb95d6d7369dee1321e297e634b4864b74
Nadime Barhoumi <nadime@riscv.org>
2026-06-16 02:04:04 -0400

    Use fakeroot instead of mknod

--- linux.mk
+++ linux.mk
@@ -35,12 +35,11 @@ $(LINUX_IMAGE): $(LINUX_CONFIG) $(LINUX_INITRAMFS)
 	cp linux/build/arch/riscv/boot/Image $(LINUX_IMAGE)
 
 $(LINUX_INITRAMFS): $(BBOX_CONFIG) disks/linux_initramfs/init
-	-mknod -m 666 disks/linux_initramfs/dev/null c 1 3
-	-mknod -m 600 disks/linux_initramfs/dev/console c 5 1
 	cp $(BBOX_CONFIG) busybox/.config
 	$(MAKE) -C busybox ARCH=riscv CROSS_COMPILE=$(CROSS_COMPILE) -j$$(nproc)
 	$(MAKE) -C busybox ARCH=riscv CROSS_COMPILE=$(CROSS_COMPILE) install CONFIG_PREFIX=../disks/linux_initramfs/
-	cd disks/linux_initramfs && find . -print0 | cpio --null -ov --format=newc --owner root:root > ../../$(LINUX_INITRAMFS)
+	fakeroot sh -c 'rm -f disks/linux_initramfs/dev/null disks/linux_initramfs/dev/console; mknod -m 666 disks/linux_initramfs/dev/null c 1 3; mknod -m 600 disks/linux_initramfs/dev/console c 5 1; cd disks/linux_initramfs && find . -print0 | cpio --null -ov --format=newc --owner root:root > ../../$(LINUX_INITRAMFS)' \
+	| cpio --null -ov --format=newc --owner root:root > ../../$(LINUX_INITRAMFS)'
 
 $(TARGETDIR)/%.dtb: %.dts
 	dtc $< > $@
